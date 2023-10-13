c4b1c76e68783ac29e71045d57323b6e250224a4
SanskarGour <112721757+SanskarGour@users.noreply.github.com>
2023-10-13 12:12:19 +0530

    Add files via upload

---
+++ Razerpay-clone/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["*"],
+theme: {
+  extend: {
+    fontFamily: {
+      mullish:["mullish","sans-serif"],
+    },
+    colors:{
+      featureBlue: "rgba(25,28,47,255)",
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      greyText: "#818597",
+      lightGrey: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+    },
+  },
+},
+plugins: [],
+}
+
